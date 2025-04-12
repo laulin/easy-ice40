663b2c46db9976d65b7764246113b8640e8fb5cd
Laulin <gignops@gmail.com>
2025-04-12 17:24:11 +0200

    remove comments

--- example/blink_tb.v
+++ example/blink_tb.v
@@ -5,24 +5,21 @@ module blink_tb;
     wire led1;
     wire lcol1;
 
-    // Instanciation du module blink
     blink uut (
         .clk12MHz(clk12MHz),
         .led1(led1),
         .lcol1(lcol1)
     );
 
-    // Génération de l'horloge 12 MHz
     initial begin
         clk12MHz = 0;
-        forever #41.666 clk12MHz = ~clk12MHz; // Période de 83.333 ns pour 12 MHz
+        forever #41.666 clk12MHz = ~clk12MHz; 
     end
 
-    // Surveillance des signaux
     initial begin
         $dumpfile("blink_tb.vcd");
         $dumpvars(0, blink_tb);
-        #500000000; // Durée de simulation : 500 ms
+        #500000000; 
         $finish;
     end
 endmodule
